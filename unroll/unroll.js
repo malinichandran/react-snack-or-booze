9280d3f9274dfa8d9fe91c7a01582e4cb925892c
malini chandran <malinichandran@Malinis-MacBook-Air.local>
2021-07-19 22:27:32 -0700

    unroll code submission

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,38 @@
-function unroll(squareArray) {
+const square = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+  ];
+  
+  const smallerSquare = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"]
+  ];
+  
+  const unRoll = (square) => {
+  let resArr = [];
+  resArr = [...square[0]];
+  for( i = 1; i<square.length-1; i++){
+    resArr = [...resArr, square[i][square[i].length-1]];
+  }
+  resArr = [...resArr, ...square[square.length-1].reverse()];
+  for( j = square.length-2; j>0 ; j--){
+    resArr = [...resArr, square[j][0]];
+  }
+  for(k = 1; k< square[1].length-1; k++){
+    resArr = [...resArr, square[1][k]];
+  }
+  if(square.length > 3){
+      for(x = square.length-2; x<square.length-1; x++){
+          resArr = [...resArr, square[x][square[x].length-2], square[x][square[x].length-3]];
+  }
+  }
+  return resArr;
+  }
+  console.log(unRoll(square));
+  console.log(unRoll(smallerSquare));
+  
 
-}
-
-module.exports = unroll;
+module.exports = unRoll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -1,9 +1,18 @@
-const unroll = require("./unroll");
+const unRoll = require("./unroll");
 
-describe("#unroll", function () {
+describe("#unRoll", function () {
 
   it("is a function", function () {
-    expect(typeof unroll).toEqual("function");
+    expect(typeof unRoll).toEqual("function");
   });
 
+  it("returns an array as result", function(){
+    expect(typeof unRoll([[1,2,3],[4,5,6],[7,8,9]])).toEqual("object")
+  });
+
+  it("check if the function works properly", function(){
+    expect(unRoll([[1,2,3],[4,5,6],[7,8,9]])).toEqual([1,2,3,6,9,8,7,4,5])
+  });
+
+  
 });
